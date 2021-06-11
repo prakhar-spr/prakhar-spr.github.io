54186d2933cf3ef66c3bf87e9bc5bc638f053c86
prakhar-spr <85689312+prakhar-spr@users.noreply.github.com>
2021-06-11 10:47:07 +0530

    Directly obtain images from web.
    Images now directly obtained from links on internet.
    Code made more scalable, now image links and titles are directly stored in array of objects.

--- script.js
+++ script.js
@@ -1,7 +1,64 @@
+const imgInfo = [
+  {
+    previewImage:
+      "https://images.unsplash.com/photo-1561948955-570b270e7c36?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80",
+    title: "cat.jpeg",
+  },
+  {
+    previewImage:
+      "https://images.unsplash.com/photo-1606787620819-8bdf0c44c293?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80",
+    title: "cooking couple shoot portofilio(1).jpg",
+  },
+  {
+    previewImage:
+      "https://images.unsplash.com/photo-1539367628448-4bc5c9d171c8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80",
+    title: "bali-kelingking-beach-plastic-removal-drive.key",
+  },
+  {
+    previewImage:
+      "https://images.unsplash.com/photo-1623206837956-07dab21608f6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80",
+    title: "NextByk Investor Pitch 2021.ppt",
+  },
+  {
+    previewImage:
+      "https://images.unsplash.com/photo-1526628953301-3e589a6a8b74?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80",
+    title: "interns-performance-report-june-2021.key",
+  },
+];
+
+let mainImg = document.querySelector(".main_image img");
+mainImg.setAttribute("id", "main");
+mainImg.setAttribute("src", imgInfo[0].previewImage); // initializing the main image to the first image of ImgInfo array.
+
+let iter = 0; // iterator for marking the current position of the element in ImgInfo array.
+
+let division = document.querySelector(".sideBar"); // select the divison which will contain all the sideBar images and their captions
+
+// for each element contained in the imgInfo Array, adding a Corresponding child divison which will in turn have image tag and paragraph tag as its attribute.
+imgInfo.forEach((element) => {
+  let sideDiv = document.createElement("div"); //create a new divison
+  sideDiv.setAttribute("id", `sideDiv${iter}`);
+  sideDiv.classList.add("sideDiv");
+  let sideImg = document.createElement("img"); // craete image tag to add image in sideBar
+  sideImg.classList.add("thumb");
+  sideImg.setAttribute("id", `sideImg${iter}`);
+  sideImg.setAttribute("src", element.previewImage);
+  sideImg.setAttribute("onclick", "change(this.src,this.id)");
+  sideDiv.append(sideImg); // appending image to divison
+  let sideCap = document.createElement("p"); // craeting paragraph to decribe the image caption
+  sideCap.classList.add("desc");
+  sideCap.innerHTML = element.title;
+  sideDiv.append(sideCap); // appending p tag to divison
+  division.append(sideDiv); // appending the new divison to its parent divison.
+  iter++;
+});
+
 let i = 0; // iterator which Represent  the current selected element. i varries from 0 to 4 representing all the 5 images in sidebar
 
 sideBar = document.querySelectorAll(".sideBar img"); // List of all the image nodes of sidebar
 sideBarDiv = document.querySelectorAll(".sideBar div"); // List of all the division tags of sidebar which house images and paragraphs
+
+sideBarDiv[0].classList.add("blue");
 
 //function triggered on mouse click on image:
 const change = (src, imgId) => {
